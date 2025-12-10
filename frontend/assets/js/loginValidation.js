9028b6843b762b730ef00109e922647edbeba250
Erin <erinabduli7@gmail.com>
2025-12-10 23:55:45 +0100

    added basic login form validation with vanilla js'

---
+++ frontend/assets/js/loginValidation.js
@@ -0,0 +1,33 @@
+const form = document.querySelector(".login-form");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
+const emailErrorMsg = document.querySelector(".emailErrorMsg");
+const passwordErrorMsg = document.querySelector(".passwordErrorMsg");
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const email = emailInput.value.trim();
+	const password = passwordInput.value.trim();
+
+	emailInput.style.border = "";
+	passwordInput.style.border = "";
+
+	let valid = true;
+
+	const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	if (!emailPattern.test(email)) {
+		console.log("Email jo valid");
+		emailInput.style.border = "2px solid red";
+		valid = false;
+	}
+
+	if (password.length < 8) {
+		console.log("Passwordi duhet te jete me i madh se 8");
+		passwordInput.style.border = "2px solid red";
+		valid = false;
+	}
+
+	if (valid) {
+		form.submit();
+	}
+});
